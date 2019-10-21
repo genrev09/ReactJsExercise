4a9a8f892a6355a972da12db9e0973657798a735
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-21 12:18:14 +0800

    - Converted function App to class App. - Added functionality to increment number per click.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={
-          ()=> {alert('now you know how to handle onclick event');}
-        }> Hello World! </button>
-        <p>Number: 0</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {Number: 0};
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={
+            ()=> {alert('now you know how to handle onclick event');
+            this.setState({Number: this.state.Number += 1});
+          }
+          }> Hello World! </button>
+          <p>Number: {this.state.Number}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
